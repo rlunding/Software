210c2f87e4b594121812117b8606452f7bdc1413
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-08-29 15:42:51 +0200

    reg

--- catkin_ws/src/00-infrastructure/easy_regression/include/easy_regression/__init__.py
+++ catkin_ws/src/00-infrastructure/easy_regression/include/easy_regression/__init__.py
@@ -3,5 +3,5 @@ from .processor_interface import *
 from .analyzers import *
 from .processors import *
 from .regression_test import *
-from easy_regression.conditions.result_db import ResultDBEntry
-from easy_regression.cli.db_yaml import rdbe_from_yaml
+from .conditions.result_db import ResultDBEntry
+from .cli.db_yaml import rdbe_from_yaml
