6381ac098d483397bf55e5cf305f489be0f58e3e
Fabio Meier <fabitosh@gmail.com>
2017-12-20 10:02:27 +0100

    obstacleCallback strucutre added

--- catkin_ws/src/25-devel-saviors/obst_avoid/src/obstacle_avoidance_node.py
+++ catkin_ws/src/25-devel-saviors/obst_avoid/src/obstacle_avoidance_node.py
@@ -9,73 +9,84 @@ from geometry_msgs.msg import PoseArray
 from obst_avoid.detector import Detector
 from duckietown_utils import get_base_name, rgb_from_ros, rectify, load_camera_intrinsics
 
+
 class ObstAvoidNode(object):
     def __init__(self):
         self.node_name = "Obstacle Avoidance Node"
         robot_name = rospy.get_param("~robot_name", "")
         self.count = 1
 
-        self.detector = Detector(robot_name=robot_name)
-
-        # Load camera calibration parameters
-	    #self.intrinsics = load_camera_intrinsics(robot_name)
+        self.detector = Detector(robot_name=robot_name)  # not sure what that does
 
         ########################
         ###### Publishers ######
         # Emergency brake to be triggered iff == 1
-        self.pub_topic = '/{}/obst_avoid/brake'.format(robot_name)
+        self.pub_topic = 'obstacle_emergency_stop_flag'.format(robot_name)
         self.brake_pub = rospy.Publisher(self.pub_topic, Binary, queue_size=1)
 
-        #ToDo: Think about the implementation of a binary publisher to say if d_desired should be read out
+        self.pub_topic = 'obstacle_avoidance_active_flag'.format(robot_name)
+        self.brake_pub = rospy.Publisher(self.pub_topic, Binary, queue_size=1)
+
         # Desired d. Only read when Obstacle is detected
         self.pub_topic = '/{}/obst_avoid/d_desired'.format(robot_name)
-        self.publisher = rospy.Publisher(self.pub_topic, Float32 , queue_size=1)
-        #ToDo: Consider Float32MultiArray if theta will be used.
+        self.publisher = rospy.Publisher(self.pub_topic, Float32, queue_size=1)
+        # ToDo: Consider Float32MultiArray if theta will be used.
         # Desired theta for dev-controllers to tune the controls
         self.pub_topic = '/{}/obst_avoid/theta_desired'.format(robot_name)
-        self.publisher = rospy.Publisher(self.pub_topic, Float32, queue_size = 1)
+        self.publisher = rospy.Publisher(self.pub_topic, Float32, queue_size=1)
 
         ########################
         ###### Subscribers #####
-        #ToDo: d_current, theta_current
+        self.sub_topic = '/{}/obst_detect/posearray'.format(robot_name)
+        self.subscriber = rospy.Subscriber(self.sub_topic, PoseArray, self.obstacleCallback)
+
+        # ToDo: d_current, theta_current
         self.sub_topic = '/{}/localization_node/'.format(robot_name)
-        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.callback)
+        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.dCallback)
+        self.sub_topic = '/{}/localization_node/'.format(robot_name)
+        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.thetaCallback)
 
-        #ToDo: intersection
+        # ToDo: intersection
         self.sub_topic = '/{}/'.format(robot_name)
-        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.callback)
+        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.intersectionCallback)
 
-        #ToDo: line_detection
+        # ToDo: line_detection
         self.sub_topic = '/{}/Anti_Instagram'.format(robot_name)
-        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.callback)
+        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.lineCallback)
 
-
-    def callback(self, image):
-        if (self.count==10): #only run with 30/self.count Hz
-            
-            obst_image = CompressedImage()
-            obst_image.header.stamp = image.header.stamp
-            obst_image.format = "jpeg"
-
-            # pass RECTIFIED IMAGE TO DETECTOR MODULE
-            obst_image.data = self.detector.process_image(rectify(rgb_from_ros(image),self.intrinsics))
-
-            #later instead of obst_image-data more like:
-            #1. EXTRACT OBSTACLES 
-            #2. SEND THEM
-            #3. VISUALIZE THEM!
-
-            # publish new message
-            self.publisher.publish(obst_image.data)
-            self.count=1
+    def obstacleCallback(self, obstacle_poses):
+        amount_obstacles = len(obstacle_poses)
+        amount_obstacles_on_track = 0;
+        for x in range(0, amount_obstacles, 1):
+            if obstacle_poses[0].pose.z > 0:
+                amount_obstacles_on_track+=1
+        if amount_obstacles_on_track = 0:
+            return
+        if amount_obstacles_on_track = 1:
+            avoid()
         else:
-            self.count+=1
-  
+            self.brake_pub.publish(1);
+
+        return
+
+    def dCallback(self):
+        return
+
+    def thetaCallback(self):
+        return
+
+    def thetaCallback(self):
+        return
+
+    def intersectionCallback(self):
+        return
+
+    def lineCallback(self):
+        return
 
     def onShutdown(self):
         rospy.loginfo('Shutting down Obstacle Detection, back to unsafe mode')
 
-# MEINER MEINUNG NACH HIER DANN WARSCH 2.NODE AUCH NOCH REIN WO DANN DIE OBST AVOIDANCE GEMACHT WIRD ODER SO
 
 if __name__ == '__main__':
     rospy.init_node('obst_detection_node', anonymous=False)
